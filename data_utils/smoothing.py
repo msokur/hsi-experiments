72c5302d7ef6d22370679a67ddfc324f97508f4a
mariia.tkachenko@medizin.uni-leipzig.de <mi186veva@login01.sc.uni-leipzig.de>
2022-07-29 20:02:29 +0200

    Smoothing. Part 1

---
+++ data_utils/smoothing.py
@@ -0,0 +1,42 @@
+from scipy.ndimage import gaussian_filter, median_filter
+import abc
+from tqdm import tqdm
+from glob import glob
+import os
+import numpy as np
+
+import config
+
+class Smoother:
+    def __init__(self, path, size=config.SMOOTHING_VALUE):
+        self.path = path
+        self.size = size
+    
+    @abc.abstractmethod
+    def smooth_func(self, X):
+        return
+    
+    def smooth(self):
+        paths = glob(os.path.join(self.path, '*.npz'))
+        
+        for path in tqdm(paths):
+            data = np.load(path)
+            X = self.smooth_func(data['X'])
+            data_ = {n: a for n, a in data.items()}
+            data_['X'] = X.copy()
+            np.savez(path, **data_)
+            
+    
+class MedianFilter(Smoother):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+    def smooth_func(self, X):
+        return median_filter(X, size=self.size)
+
+class GaussianFilter(Smoother):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+    def smooth_func(self, X):
+        return gaussian_filter(X, self.size)
