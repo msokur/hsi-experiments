50608304dce7aa2c5f1548ecb057119a76538462
Benjamin Huber <benny-37@web.de>
2023-05-09 10:10:54 +0200

    fix memory error in scaler.py

--- data_utils/scaler.py
+++ data_utils/scaler.py
@@ -1,4 +1,6 @@
 import abc
+import gc
+
 import numpy as np
 import os
 from tqdm import tqdm
@@ -11,9 +13,11 @@ from data_utils.data_loaders.data_loader_dyn import DataLoaderDyn
 
 
 class Scaler:
-    def __init__(self, preprocessed_path, scaler_file=None, scaler_path=None):
+    def __init__(self, preprocessed_path, scaler_file=None, scaler_path=None, dict_names=None):
         self.preprocessed_path = preprocessed_path
         self.scaler_path = scaler_path
+        if dict_names is None:
+            self.dict_names = ["X", "y", "indexes_in_datacube"]
         
         if self.scaler_path is not None: 
             self.scaler = Scaler.scaler_restore(self.scaler_path)
@@ -39,20 +43,27 @@ class Scaler:
     def X_y_concatenate(self):
         paths = glob(os.path.join(self.preprocessed_path, '*.npz'))
 
-        X, y, indexes = [], [], []
+        X_s, y_s, indexes_s = self.get_shapes(paths[0])
+        X, y, indexes = np.empty(shape=X_s), np.empty(shape=y_s), np.empty(shape=indexes_s)
         for path in paths:
             data = np.load(path)
-            _X, _y, _i = data['X'], data['y'], data['indexes_in_datacube']
+            _X, _y, _i = data[self.dict_names[0]], data[self.dict_names[1]], data[self.dict_names[2]]
 
             # check if data 3D
             if len(np.array(_X).shape) > 2:
-                _X = DistributionsChecker.get_centers(_X)
+                _X_1d = DistributionsChecker.get_centers(_X)
+            else:
+                _X_1d = _X
 
-            X += list(_X)
-            y += list(_y)
-            indexes += list(_i)
-
-        X, y, indexes = np.array(X), np.array(y), np.array(indexes)
+            X = np.concatenate((X, _X_1d), axis=0)
+            y = np.concatenate((y, _y), axis=0)
+            indexes = np.concatenate((indexes, _i), axis=0)
+            del _X
+            del _y
+            del _i
+            del _X_1d
+            del data
+            gc.collect()
 
         return X, y, indexes
 
@@ -90,11 +101,16 @@ class Scaler:
 
         for path in tqdm(paths):
             data = np.load(path)
-            X = data['X']
+            X = data[self.dict_names[0]]
             X = self.scale_X(X)
             data = {n: a for n, a in data.items()}
-            data['X'] = X.copy()
+            data[self.dict_names[0]] = X.copy()
             np.savez(os.path.join(destination_path, DataLoaderDyn().get_name(path)), **data)
+
+    def get_shapes(self, path):
+        datas = np.load(path)
+        X, y, idx = datas[self.dict_names[0]].shape, datas[self.dict_names[1]].shape, datas[self.dict_names[2]].shape
+        return [0, X[-1]], [0] if len(y) == 1 else [0, y[-1]], [0, idx[-1]]
             
 
 class NormalizerScaler(Scaler):
