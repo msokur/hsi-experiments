ad51142b2d64f88b03263be328f4f22415f74b3f
INFORMATIK\tkachenko <tkachenko@informatik.uni-leipzig.de>
2023-06-21 11:56:58 +0200

    Refactored Validator (created OptimalThreshold and MetricsReaders except)

---
+++ evaluation/metrics_csvreader.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import numpy as np
+
+class MetricsCsvReader:
+    def fill_result(self, result, name, stacked_metrics):
+        result[name] = {}
+        result[name]['metrics'] = stacked_metrics[:-3]
+        result[name]['mean'] = stacked_metrics[-3]
+        result[name]['std'] = stacked_metrics[-2]
+        result[name]['median'] = stacked_metrics[-1]
+    
+    def stack_columns(self, data, suitable_columns):
+        return [np.array(data[column].tolist()).astype(float) for column in suitable_columns]
+        
+    def read_metrics(self, csvfile, names=[]):
+        data = pd.read_csv(csvfile)
+        columns = data.columns
+        
+        result = {}
+        for name in names:
+            suitable_columns = [column for column in columns if name in column]
+            stacked_columns = self.stack_columns(data, suitable_columns)
+
+            if len(suitable_columns) > 1:
+                stacked_metrics = np.array([list(row) for row in zip(*stacked_columns)])
+            else:
+                stacked_metrics = stacked_columns[0]
+            
+            self.fill_result(result, name, stacked_metrics)
+            
+        return result
+
+class MetricsCsvReaderComparisonFiles(MetricsCsvReader):   #for compare_all_thresholds*.csv files
+    def fill_result(self, result, name, stacked_metrics):
+        result[name] = stacked_metrics
+    
+    def stack_columns(self, data, suitable_columns):
+        def convertable_to_float(string):
+            try:
+                result = float(string)
+                return True
+            except ValueError:
+                return False
+            
+        stacked_columns = []
+        for column in suitable_columns:
+            rows = list(data[column])
+            for row_i, row in enumerate(rows):
+                if not convertable_to_float(row):
+                    row = row.replace('[','')
+                    row = row.replace(']','')
+                    row = row.split(' ')
+                    lst = [float(elem) for elem in row if elem != ''] 
+                    rows[row_i] = lst
+                stacked_columns.append(rows)
+        return stacked_columns
+
+
+    
+if __name__ == '__main__':
+    reader = MetricsCsvReader()
+    print(reader.read_metrics('/home/sc.uni-leipzig.de/mi186veva/hsi-experiments/test/Esophagus_MedFilter/cp-0038/metrics_by_threshold_None.csv', names=['Sensitivity', 'Specificity', 'Accuracy']))
+    #reader = MetricsCsvReaderComparisonFiles()
+    #reader.read_metrics('/home/sc.uni-leipzig.de/mi186veva/hsi-experiments/test/Esophagus_MedFilter/cp-0000/compare_all_thresholds.csv', names=['Sensitivity', 'Specificity'])
--- evaluation/optimal_parameters.py
+++ evaluation/optimal_parameters.py
@@ -4,10 +4,16 @@ import csv
 import os
 import numpy as np
 from tqdm import tqdm
+import pandas as pd
 
+import inspect
+import sys
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir) 
 import config
 
-class Validator():
+class OptimalThreshold():
     
     def __init__(self, root_path,
                          checkpoints_regex='cp-0000'+config.SYSTEM_PATHS_DELIMITER,
@@ -53,7 +59,6 @@ class Validator():
         return data, sensitivity_column, specificity_column, threshold_column
 
     def get_thresholds_sens_spec(self, data, sensitivity_column, specificity_column, threshold_column):
-        # get data
         thr = data[:, threshold_column].astype(np.float32)
         sens = data[:, sensitivity_column].astype(np.float32)
         spec = data[:, specificity_column].astype(np.float32)
@@ -66,7 +71,7 @@ class Validator():
         idx = thr == threshold
         return sens[idx], spec[idx], np.mean([sens[idx], spec[idx]]), idx
 
-    def find_best_threshold_in_checkpoint(self, checkpoint_folder):
+    def find_optimal_threshold_in_checkpoint(self, checkpoint_folder):
         data, sensitivity_column, specificity_column, threshold_column = self.get_data(checkpoint_folder)
 
         thr, sens, spec = self.get_thresholds_sens_spec(data, sensitivity_column, specificity_column, threshold_column)
@@ -96,13 +101,18 @@ class Validator():
         idx = np.argmin(diff).flatten() + 1
 
         if self.prints:
-            print(f'Best index and threshold for {checkpoint_folder}: ', idx, thr[idx])
-            print(f'Best sensitivity and specificity for {checkpoint_folder}: ', sens[idx], spec[idx])
-            print(f'Mean value of best sensitivity and specificity for {checkpoint_folder}: ', np.mean([sens[idx], spec[idx]]))
+            print(f'optimal index and threshold for {checkpoint_folder}: ', idx, thr[idx])
+            print(f'optimal sensitivity and specificity for {checkpoint_folder}: ', sens[idx], spec[idx])
+            print(f'Mean value of optimal sensitivity and specificity for {checkpoint_folder}: ', np.mean([sens[idx], spec[idx]]))
 
         return thr[idx], sens[idx], spec[idx], np.mean([sens[idx], spec[idx]]), idx
     
-    def find_best_checkpoint(self, ):
+    
+class OptimalCheckpoint(OptimalThreshold):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+    def find_optimal_checkpoint(self):
 
         checkpoints = glob.glob(os.path.join(self.root_path, self.checkpoints_regex))
         if self.prints:
@@ -112,29 +122,32 @@ class Validator():
 
         thresholds = []
         means = []
-        best_sens = []
-        best_spec = []
+        optimal_sens = []
+        optimal_spec = []
 
         for checkpoint in tqdm(checkpoints):
-            threshold, sens, spec, mean, idx = self.find_best_threshold_in_checkpoint(checkpoint)
-            best_sens.append(sens)
-            best_spec.append(spec)
+            threshold, sens, spec, mean, idx = self.find_optimal_threshold_in_checkpoint(checkpoint)
+            optimal_sens.append(sens)
+            optimal_spec.append(spec)
             thresholds.append(threshold)
             means.append(mean)
         
         
-        best_idx = np.nanargmax(means)
-        best_checkpoint = checkpoints[best_idx]
-        best_threshold = thresholds[best_idx]
+        optimal_idx = np.nanargmax(means)
+        optimal_checkpoint = checkpoints[optimal_idx]
+        optimal_threshold = thresholds[optimal_idx]
 
         if self.prints:
-            print(f'Best index: {best_idx}, best checkpoint: {best_checkpoint}, best_threshold: {best_threshold}')
+            print(f'optimal index: {optimal_idx}, optimal checkpoint: {optimal_checkpoint}, optimal_threshold: {optimal_threshold}')
             print('Means:', means)
             print('------------------------------------------------------')
         
-        return best_checkpoint, best_threshold, thresholds, means, best_sens[best_idx], best_spec[best_idx]
+        return optimal_checkpoint, optimal_threshold, thresholds, means, optimal_sens[optimal_idx], optimal_spec[optimal_idx]
+    
+          
+            
 
     
 if __name__ == '__main__':
-    validator = Validator()
-    validator.find_best_checkpoint('/home/sc.uni-leipzig.de/mi186veva/hsi-experiments/test/CV_3d_inception/')
+    reader = MetricsCsvReader()
+    reader.read_metrics('/home/sc.uni-leipzig.de/mi186veva/hsi-experiments/test/Esophagus_MedFilter/cp-0038/metrics_by_threshold_None.csv', names=['Sensitivity', 'Specificity'])
