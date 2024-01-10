2f5546b48a484d1b665ec95069b4ce73a29d93f7
Benjamin Huber <benny-37@web.de>
2024-01-10 10:35:17 +0100

    add comments

--- data_utils/tfrecord/tfr_utils.py
+++ data_utils/tfrecord/tfr_utils.py
@@ -72,6 +72,19 @@ def parse_names_to_int(tfr_files: list) -> Dict[str, int]:
 
 
 def filter_name_idx_and_labels(X, y, sw, pat_idx, use_pat_idx: tf.Variable, use_labels: tf.Variable):
+    """Filter function for a tfrecord dataset
+
+     Filters the variables X, y and sw by patient indexes and labels.
+
+    :param X: List with trainings data
+    :param y: List with labels
+    :param sw: List with sample weights
+    :param pat_idx: List with patient indexes
+    :param use_pat_idx: patient index to filter
+    :param use_labels: labels to filter
+
+    :return: Filtered X, y and sw
+    """
     idx_mask = _select(data=pat_idx, allowed=use_pat_idx)
     label_mask = _select(data=y, allowed=use_labels)
     mask = tf.logical_and(x=idx_mask, y=label_mask)
@@ -145,6 +158,8 @@ def _get_section_from_meta(file_path: str, section: str) -> dict:
     return info[section]
 
 
-def _select(data, allowed):
+def _select(data: tf.Variable, allowed: tf.Variable):
+    # get a boolean list for every element in allowed
     use = tf.map_fn(fn=lambda com: tf.equal(x=data, y=com), elems=allowed, fn_output_signature=tf.bool)
+    # combine the lists with a logical or
     return tf.cast(tf.math.reduce_sum(input_tensor=tf.cast(use, dtype=tf.int32), axis=0), dtype=tf.bool)
