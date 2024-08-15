5429e8d79011d477528ce61ee53443df0e9675a7
Benjamin Huber <benjamin.huber@hs-anhalt.de>
2024-08-15 10:24:23 +0200

    some formatting stuff in meta_config

--- configuration/meta_configs/Benny.py
+++ configuration/meta_configs/Benny.py
@@ -1,16 +1,29 @@
+# --- Data Loader configurations ---
 loader_config_file = "DataLoader.json"
 loader_section = "HNO_2"
+
+# --- Path configurations ---
 path_config_file = "Paths.json"
 system_section_cluster = "Cluster_Benny"
 system_section_local = "Win_Benny"
 database_section = "HNO_2_Database"
+
+# --- Preprocessing configurations ---
 prepro_config_file = "Preprocessor.json"
 prepro_section = "GASTRIC"
+
+# --- Cross Validation configuration ---
 cv_config_file = "Crossvalidation.json"
 cv_section = "CV_GASTRIC_MK"
+
+# --- Trainer configuration ---
 trainer_config_file = "Trainers.json"
 trainer_section = "BASE_BINARY"
+
+# --- Distribution checking configuration ---
 distro_config_file = "DistributionsCheck.json"
 distro_section = "Default"
+
+# --- Telegram configuration ---
 tg_config_file = "Telegram.json"
 tg_section = "Benny"
--- configuration/meta_configs/Mariia.py
+++ configuration/meta_configs/Mariia.py
@@ -1,16 +1,29 @@
+# --- Data Loader configurations ---
 loader_config_file = "DataLoader.json"
-loader_section = "COLON"   # DataLoader.json
+loader_section = "COLON"
+
+# --- Path configurations ---
 path_config_file = "Paths.json"
-system_section_cluster = "Cluster_Mariia"   # Paths.json
-system_section_local = "Win_Mariia"    # Paths.json
-database_section = "COLON"    # Paths.json
+system_section_cluster = "Cluster_Mariia"
+system_section_local = "Win_Mariia"
+database_section = "COLON"
+
+# --- Preprocessing configurations ---
 prepro_config_file = "Preprocessor.json"
-prepro_section = "COLON"   # Preprocessor.json
+prepro_section = "COLON"
+
+# --- Cross Validation configuration ---
 cv_config_file = "Crossvalidation.json"
 cv_section = "CV_COLON"
+
+# --- Trainer configuration ---
 trainer_config_file = "Trainers.json"
 trainer_section = "INCEPTION_BINARY"
+
+# --- Distribution checking configuration ---
 distro_config_file = "DistributionsCheck.json"
 distro_section = "Default"
+
+# --- Telegram configuration ---
 tg_config_file = "Telegram.json"
 tg_section = "Mariia"
